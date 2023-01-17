73f60149b881f86816d555fdc108c746bc4020dd
Лиза Юрьева <lizanka91@mail.ru>
2023-01-18 02:03:59 +0300

    My 4 spnt

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -129,4 +129,4 @@ POSTS_ON_SCREEN = 10
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails') 
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
